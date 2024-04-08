e5477b497c0ef6fa060deac2314256dcf6b5002a
lruane00 <166447123+lruane00@users.noreply.github.com>
2024-04-08 16:52:39 -0400

    Add files via upload

---
+++ R Script for Clustering in Tableau.R
@@ -0,0 +1,11 @@
+if(!require(Rserve)) install.packages("Rserve", "Rserve_1.8-6.tgz", "http://www.rforge.net/")
+library(Rserve)
+
+
+if (!require(cluster)) install.packages("cluster")
+library(cluster)
+if (!require(factoextra)) install.packages("factoextra")
+library(factoextra)
+
+
+Rserve(args = "--no-save")
